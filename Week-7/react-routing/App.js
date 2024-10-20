37e9747e26a68ef6755ba50bb84c3483165dbd62
JAWERIYAKHAN26 <Jaweriyakhanofficial@gmail.com>
2024-10-20 19:27:52 +0500

    Week 8 tasks compleetd

---
+++ Week-7/react-routing/App.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import HomeScreen from "./screens/home";
+import DetailsScreen from "./screens/details";
+import About from "./screens/about";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+        <Stack.Screen name="About" component={About}></Stack.Screen>
+        <Stack.Screen name="Details" component={DetailsScreen}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ Week-8/react-navigation/assets/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+const globleStyles = StyleSheet.create({
+  container: {},
+  txt: {
+    color: "yellow",
+  },
+});
+
+export default globleStyles;
